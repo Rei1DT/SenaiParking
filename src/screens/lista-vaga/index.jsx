7e6e73ca59a5f5757a4d701f4e7f864f23c9efb9
Rei1DT <reinaldo.s.santos8@aluno.senai.br>
2025-10-22 17:12:40 -0300

    feat: api na tela de lista vaga

--- src/screens/lista-vaga/index.jsx
+++ src/screens/lista-vaga/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, Text } from "react-native";
 import { Truck, Calendar, Clock } from "react-native-feather";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { API_BASE_URL, getAxiosWithToken } from "../../api";
 import {
   Container,
   Header,
@@ -15,16 +17,22 @@ import {
   ButtonText
 } from "./style";
 
-const vagas = [
-  { id: "A1", placa: "ABC-1234", data: "09/24/2025", hora: "14:30:00" },
-  { id: "A2", placa: "DEF-5678", data: "09/25/2025", hora: "08:15:00" },
-  { id: "A3", placa: "GHI-9101", data: "09/26/2025", hora: "10:45:00" },
-  { id: "A4", placa: "JKL-2345", data: "09/27/2025", hora: "11:30:00" },
-  { id: "A5", placa: "MNO-6789", data: "09/28/2025", hora: "13:00:00" },
-];
-
 export default function VagasScreen() {
   const navigation = useNavigation();
+  const [vagas, setVagas] = useState([]);
+
+  useEffect(() => {
+    async function fetchVagas() {
+      try {
+        const axiosInstance = await getAxiosWithToken();
+        const response = await axiosInstance.get('/api/veiculos');
+        setVagas(response.data);
+      } catch (error) {
+        setVagas([]);
+      }
+    }
+    fetchVagas();
+  }, []);
 
   return (
     <Container>
@@ -34,8 +42,8 @@ export default function VagasScreen() {
       </Header>
 
       <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
-        {vagas.map((vaga) => (
-          <VagaItem key={vaga.id}>
+        {vagas.map((vaga, idx) => (
+          <VagaItem key={idx}>
             <IconContainer>
               <Truck width={24} height={24} color="white" />
             </IconContainer>
@@ -45,10 +53,10 @@ export default function VagasScreen() {
                 <Calendar width={16} height={16} color="white" /> Placa: {vaga.placa}
               </Text>
               <Text style={{ color: "white", fontSize: 14 }}>
-                <Calendar width={16} height={16} color="white" /> Data: {vaga.data}
+                <Calendar width={16} height={16} color="white" /> Data: {vaga.dataEntrada}
               </Text>
               <Text style={{ color: "white", fontSize: 14 }}>
-                <Clock width={16} height={16} color="white" /> Hora: {vaga.hora}
+                <Clock width={16} height={16} color="white" /> Hora: {vaga.horarioEntrada}
               </Text>
             </TextContainer>
 
